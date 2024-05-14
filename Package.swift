507cfb17769ead6bb9d48a7e30f49fda9e0ddffa
Drew Baumann <drewbaumann@gmail.com>
2024-05-13 17:03:48 -0700

    chore: Adds package support for visionOS

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.5
+// swift-tools-version:5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "EasyMenu",
     platforms: [
-        .iOS(.v14),
+        .iOS(.v14), .visionOS(.v1)
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
